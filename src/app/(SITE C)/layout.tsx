9d9ac760a915484125486c1964e660a8520e04ea
Roshan SaiHam Nafiz <118830871+iamroshansaihamnafiz@users.noreply.github.com>
2024-04-05 12:15:22 +0600

    Site C culture page done

---
+++ src/app/(SITE C)/layout.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import '@/./app/globals.css';
+import {ToastContainer} from "react-toastify";
+import Header from "@/app/(SITE C)/Header";
+import Footer from "@/app/(SITE C)/Footer";
+
+export const metadata = {
+    title: 'Nosres',
+    description: 'SITE C',
+}
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+        <body>
+        <Header/>
+        {children}
+        <Footer/>
+        <ToastContainer
+            bodyClassName="toastBody"
+        />
+        </body>
+        </html>
+    )
+}
