d85214e0d3edbc73f074ac4dde3a930a96f11567
Roshan SaiHam Nafiz <118830871+iamroshansaihamnafiz@users.noreply.github.com>
2024-04-13 13:26:48 +0600

    Terms details page completed

--- src/app/(SITE E)/terms-details/components/Hero/page.tsx
+++ src/app/(SITE E)/terms-details/components/Hero/page.tsx
@@ -4,7 +4,7 @@ import heroImage from '@/../public/assets/images/sitee/policies-banner.jpeg';
 const Page = () => {
     return (
         <>
-            <section id="banner-section" className="bg-cover bg-no-repeat bg-center h-[400px] pt-[11rem]"
+            <section id="banner-section" className="bg-cover bg-no-repeat bg-center h-[280px] pt-[8rem]"
                      style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),url(${heroImage.src})`}}>
                 <div className="container">
                     <h1 className="text-2xl md:text-[38px] leading-0 md:leading-10 text-white font-bold md:font-medium mb-4">
