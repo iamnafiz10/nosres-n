24700838ded8c54bc609b058a9c7e4e387e8fb98
Roshan SaiHam Nafiz <118830871+iamroshansaihamnafiz@users.noreply.github.com>
2024-04-19 14:17:36 +0600

    Ready for delivery

--- src/app/(SITE G)/layout.tsx
+++ src/app/(SITE G)/layout.tsx
@@ -3,6 +3,7 @@ import '@/./app/globals.css';
 import {ToastContainer} from "react-toastify";
 import Header from "@/app/(SITE G)/Header";
 import Footer from "@/app/(SITE G)/Footer";
+import AOSProvider from "@/app/AOSProvider";
 
 export const metadata = {
     title: 'Nosres',
@@ -17,9 +18,11 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body>
-        <Header/>
-        {children}
-        <Footer/>
+        <AOSProvider>
+            <Header/>
+            {children}
+            <Footer/>
+        </AOSProvider>
         <ToastContainer
             bodyClassName="toastBody"
         />
